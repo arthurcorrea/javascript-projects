7e453e840e0260431d0ba927e5537a2f593cf829
arthurcorrea <arthurcorrea321321@gmail.com>
2023-12-15 09:47:07 -0300

    att

--- switch/src/app.js
+++ switch/src/app.js
@@ -1,4 +1,15 @@
+function randomInt(min, max) {
+    return Math.floor(Math.random() * (max - min) + min);
+}
 
+const x = randomInt(1, 5)
+console.log(x);
+
+
+
+
+
+//? ========= ROW 1 - COL 1 =========
 const buttonSwitchBackgroundColor = document.querySelectorAll('.btn-info')
 
 buttonSwitchBackgroundColor.forEach(function(e){
@@ -8,14 +19,38 @@ buttonSwitchBackgroundColor.forEach(function(e){
         backgroundColor.classList.toggle('bg-danger')
     })
 })
+// ? ================================
 
-const switchButtonColor = document.querySelectorAll('.btn-light');
 
-switchButtonColor.forEach(function(e) {
-    e.addEventListener('click', function(){
-        e.classList.toggle('btn-danger')
-    })
+//? ========= ROW 1 - COL 2 =========
+const switchButtonColor = document.querySelector('.btn-light');
+
+switchButtonColor.addEventListener('click', function(){
+    switchButtonColor.classList.toggle('btn-light')
+
+    console.log(switchButtonColor);
 })
+    
+    
+
+//? ================================
+
+
+//? ========= ROW 1 - COL 3 =========
+const buttonRow1col3 = document.getElementById('row1col3');
+
+buttonRow1col3.addEventListener('click', switchColorText);
+
+function switchColorText(e) {
+    e.preventDefault();
+
+    let paragraph = buttonRow1col3.previousElementSibling;
+
+    buttonRow1col3.classList.toggle('switchColorText')
+    paragraph.classList.toggle('switchColorText')
+}
+//? ================================
+
 // function esperar(ms) {
 //     return new Promise(resolve => setTimeout(resolve, ms));
 // }
